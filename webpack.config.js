12fd4a25c974bd1a9399bcc62a4bf7d014eedf8c
Unknown <kamil@szymon.net.pl>
2018-10-20 16:02:55 +0200

    Simple Optimizatin

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,68 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
-const config = {
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    entry: {
-        app: path.resolve(__dirname, 'src', 'index.js')
-    },
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                use: 'babel-loader'
+
+
+const config = (env, options) => {
+    const devMode = options.mode !== 'production';
+    return {
+        resolve: {
+            extensions: ['.js', '.jsx']
+        },
+        entry: {
+            app: path.resolve(__dirname, 'src', 'index.js')
+        },
+        output: {
+            filename: devMode ? '[name].bundle.js' : '[name].[hash].bundle.js',
+            path: path.resolve(__dirname, 'dist')
+        },
+        optimization: {
+            splitChunks: {
+                chunks: 'all'
             },
-            {
-                test: /\.scss$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'src', 'index.html')
-        })
-    ],
-    mode: 'development',
-    devtool: 'cheap-eval-source-map',
-    devServer: {
-        port: 3000,
-        overlay: true
+            minimizer: [
+                new UglifyJsPlugin({
+                    cache: true,
+                    parallel: true,
+                    sourceMap: true
+                  }),
+                new OptimizeCSSAssetsPlugin({})
+              ]
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    use: 'babel-loader'
+                },
+                {
+                    test: /\.scss$/,
+                    use: [
+                        devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                        'css-loader',
+                        'sass-loader'
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: path.resolve(__dirname, 'src', 'index.html')
+            }),
+            new MiniCssExtractPlugin({
+                filename: devMode ? '[name].css' : '[name].[hash].css',
+                chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+            })
+        ],
+        mode: 'development',
+        devtool: devMode ? 'cheap-eval-source-map' : 'cheap-source-map',
+        devServer: {
+            port: 3000,
+            overlay: true
+        }
     }
 };
 
