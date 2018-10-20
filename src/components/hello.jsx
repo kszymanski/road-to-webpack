a7581a31e5ddb5032423e1c2812ce2a8d57d86d1
Unknown <kamil@szymon.net.pl>
2018-10-20 18:41:03 +0200

    hello tekst

--- src/components/hello.jsx
+++ src/components/hello.jsx
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 export default class Hello extends Component {
     render(){
         return <div>
-            <h1>Krok 4 - Server developerski</h1>
-            <h2>Ta strona ma hot reload, sass'y i serwer developerski</h2>
-            <p className='how-to'>Wymaga pluginów, loaderów działa szybko, ale...</p>
+            <h1>Krok 5 - Lepszy artefakt</h1>
+            <h2>Ta ma tree shaking i kilka bundli</h2>
+            <p className='how-to'>Wymaga pluginów, loaderów i buduje się w ładny arefakt, ale...</p>
         </div>
     }
 }
